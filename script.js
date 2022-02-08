dc31b95f3ee0c8596d977ea0dac84a0d7e743065
Claire <70619108+clairetyson89@users.noreply.github.com>
2022-02-08 00:14:35 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+    require([
+      "esri/Map",
+      "esri/layers/CSVLayer",
+      "esri/views/MapView",
+      "esri/config",
+      "esri/core/urlUtils",
+      "dojo/domReady!"
+    ], function(
+      Map,
+      CSVLayer,
+      MapView,
+      esriConfig,
+      urlUtils
+    ) {
+
+     var url = "https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%202%20-%20ArcGIS%20JavaScript%20API/stl_crime_wgs_84.csv";
+     esriConfig.request.corsEnabledServers.push('https://rawgit.com');
+
+        const template = {
+          title: "Earthquake Info",
+          content: "Magnitude {mag} {type} hit {place} on {time}."
+        };
+
+        const csvLayer = new CSVLayer({
+          url: url,
+          copyright: "USGS Earthquakes",
+          popupTemplate: template
+        });
+
+        var symbol = {
+          type: "simple-marker", 
+          color: "teal"
+        };
+
+      csvLayer.renderer = {
+        type: "simple", // autocasts as new SimpleRenderer()
+        symbol: symbol
+      };
+
+      var map = new Map({
+        basemap: "gray",
+        layers: [csvLayer]
+      });
+
+var view = new MapView({
+        container: "viewDiv",
+        center: [-90.22939, 38.60675],
+        zoom: 11,
+        map: map
+      });
+
+    });
